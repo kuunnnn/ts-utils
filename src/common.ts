815bdfd2286bef8af239eb538cae61644cc153c0
胡坤 <mrhukun@gmail.com>
2020-06-23 22:50:52 +0800

    处理 node 和 browser 的模块差异

--- src/common.ts
+++ src/common.ts
@@ -1,20 +1,11 @@
 export { safeStringCompare, safeStringCompare2 } from "./compare";
 export { AES_CBC } from "./security/aes";
 export { ECDH } from "./security/dh";
-export {
-  genBCryptSalt,
-  b_crypt_hash,
-  HMAC_SHA256,
-  SHA256,
-} from "./security/hash";
-export {
-  createRsa,
-  createRsaByPrivateKey,
-  createRsaByPublicKey,
-} from "./security/rsa";
+export { genBCryptSalt, b_crypt_hash, HMAC_SHA256, SHA256 } from "./security/hash";
+export { createRsa, createRsaByPrivateKey, createRsaByPublicKey } from "./security/rsa";
 export { Regexp } from "./regexp";
-import svgCaptcha from "svg-captcha";
+
 import safeRandomBytes from "randombytes";
+import svgCaptcha from "svg-captcha";
 
-export { safeRandomBytes, svgCaptcha }
-
+export { safeRandomBytes, svgCaptcha };
--- src/node.ts
+++ src/node.ts
@@ -1,18 +1,24 @@
 export * from "./common"
 import os, { NetworkInterfaceInfo } from "os"
 
-export function ipv4Address (): string[] {
+export function ipAddress (type:"IPv4"|"IPv6") {
   const interfaces = os.networkInterfaces() as NodeJS.Dict<NetworkInterfaceInfo[]>;
   const ips = [];
   for ( let interfacesName of Object.keys( interfaces ) ) {
     for ( let networkInterface of (interfaces as any)[ interfacesName ] ) {
-      if ( 'IPv4' !== networkInterface.family || networkInterface.internal ) {
+      if ( type !== networkInterface.family || networkInterface.internal ) {
         // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
         continue;
       }
-      // 可能会有多个 ipv4 addresses
+      // 可能会有多个 addresses
       ips.push( networkInterface.address )
     }
   }
   return ips;
+}
+export function ipv4Address (): string[] {
+  return ipAddress("IPv4");
+}
+export function ipv6Address (): string[] {
+  return ipAddress("IPv6");
 }
